b2b85a68bb78b82b63e3b6a161742e9cf03ece29
Amir <amirhossein.rahimabadi@zenitech.co.uk>
2022-06-11 02:56:46 +0300

    max-age restore to 1 week

--- app/sessions.server.ts
+++ app/sessions.server.ts
@@ -4,7 +4,7 @@ export const { getSession, commitSession, destroySession } =
   createCookieSessionStorage({
     cookie: {
       name: "__session",
-      maxAge: 0,
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
       sameSite: "lax",
       path: "/",
       httpOnly: true,
