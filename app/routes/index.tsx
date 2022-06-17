0888bf468d9e7e259437a658e6c071b7034a8358
Amir <amirhossein.rahimabadi@zenitech.co.uk>
2022-06-17 08:50:52 +0300

    Dashboard

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,31 +1,79 @@
-import { Link } from "@remix-run/react"
+import type { LoaderFunction } from "@remix-run/node"
+import { redirect } from "@remix-run/node"
+import Button from "@mui/material/Button"
+import Paper from "@mui/material/Paper"
+import Box from "@mui/material/Box"
+import Grid from "@mui/material/Grid"
+import Typography from "@mui/material/Typography"
+import { Copyright } from "~/components/coypright"
+import { getUserId } from "~/session.server"
 
-import { useOptionalUser } from "~/utils/user"
+export const loader: LoaderFunction = async ({ request }) => {
+  const userID = await getUserId(request)
+  if (userID) {
+    return redirect("/dashboard")
+  }
+  return {}
+}
 
 export default function Index() {
-  const user = useOptionalUser()
   return (
-    <main>
-      {user ? (
-        <>
-          <Link to="/notes">View Notes for {user.email}</Link>
-        </>
-      ) : (
-        <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
-          <Link
-            to="/auth/signup"
-            className="flex items-center justify-center px-4 py-3 text-base font-medium text-red-700 bg-white border border-transparent rounded-md shadow-sm hover:bg-red-50 sm:px-8"
-          >
-            Sign up
-          </Link>
-          <Link
-            to="/auth/signin"
-            className="flex items-center justify-center px-4 py-3 font-medium text-white bg-red-500 rounded-md hover:bg-red-600 "
-          >
-            Log In
-          </Link>
-        </div>
-      )}
-    </main>
+    <Grid container component="main" sx={{ height: "100vh" }}>
+      <Grid
+        item
+        xs={false}
+        sm={4}
+        md={7}
+        sx={{
+          backgroundImage:
+            "url(https://source.unsplash.com/random/?technology)",
+          backgroundRepeat: "no-repeat",
+          backgroundColor: t =>
+            t.palette.mode === "light"
+              ? t.palette.grey[50]
+              : t.palette.grey[900],
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <Box
+          sx={{
+            my: 8,
+            mx: 4,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Heya!
+          </Typography>
+          <Box sx={{ mt: 1 }}>
+            <form action="/auth/signup">
+              <Button
+                type="submit"
+                fullWidth
+                variant="outlined"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign up
+              </Button>
+            </form>
+            <form action="/auth/signup">
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mb: 2 }}
+              >
+                Sign In
+              </Button>
+            </form>
+            <Copyright sx={{ mt: 5 }} />
+          </Box>
+        </Box>
+      </Grid>
+    </Grid>
   )
 }
