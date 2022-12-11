1e8ff481f2f935999061d5c18325e15d395c6a9c
Amir Ziaei <amir@amirziaei.me>
2022-12-11 23:09:39 +0200

    fix the broken e2e test

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -2,17 +2,19 @@ describe("smoke tests", () => {
   it("should allow you to login and logout", () => {
     cy.insertUser().then(user => {
       cy.visit("/")
-      cy.findByRole("button", { name: /sign in/i }).click()
+      cy.findByRole("link", { name: /sign in/i }).click()
 
-      //@ts-ignore
-      cy.findByRole("textbox", { name: /email/i }).type(user.email)
+      cy.findByRole("textbox", { name: /email/i, timeout: 20000 }).type(
+        // @ts-ignore
+        user.email,
+      )
       //@ts-ignore
       cy.findByLabelText(/password/i).type(user.password)
       cy.findByRole("button", { name: /sign in/i }).click()
 
       cy.findByRole("user-avatar", { timeout: 20000 }).click()
       cy.findByRole("button", { name: /sign out/i }).click()
-      cy.findByRole("button", { name: /sign in/i })
+      cy.findByRole("link", { name: /sign in/i })
     })
   })
 })
